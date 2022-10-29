06200320315e1e09f47e98b55278110ce21a8b6b
Vivek Gupta PC <vivekgpt107@gmail.com>
2022-10-29 21:17:25 +0530

    scrollbar

--- src/comp-home/hero.jsx
+++ src/comp-home/hero.jsx
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 import { useRef } from "react";
 import profileimg from "../assets/profile.png";
 import handwave from "../assets/waving_hand_sign_512.gif";
+import HeroProfile from "./hero-profile";
 
 const Hero = () => {
     const constraintsRef = useRef(null);
@@ -9,18 +10,7 @@ const Hero = () => {
     return (
         <div className="h-screen" ref={constraintsRef}>
             <div className="flex h-full flex-col items-center justify-center">
-                <motion.div
-                    className="cursor-move shadow-soft rounded-full overflow-hidden"
-                    drag
-                    dragConstraints={{
-                        left: -100,
-                        right: 100,
-                        top: -100,
-                        bottom: 0,
-                    }}
-                >
-                    <img draggable="false" className="h-40" src={profileimg} />
-                </motion.div>
+                <HeroProfile />
                 <div className="text-5xl leading-normal">
                     <div>
                         <div className="inline-block">Hi</div>
